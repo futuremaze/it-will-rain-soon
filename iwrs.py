1725036d83f001493ff2c2d443c6814e0c491dfc
futuremaze <future.maze@gmail.com>
2017-08-13 21:53:27 +0900

    [update] implement load_setting_file and get_weather_information

--- iwrs.py
+++ iwrs.py
@@ -10,31 +10,82 @@ YOLP(気象情報):
     https://developer.yahoo.co.jp/webapi/map/openlocalplatform/v1/weather.html
 """
 
+from datetime import datetime
+import json
+import os
 import requests
+import configparser
 
 
 def load_setting_file(file_path):
-    """ [FUNCTIONS] INI形式の設定ファイルを読み込んで辞書オブジェクト化し,
-    必須パラメタ有無のチェック, 値の範囲チェックを実施し, 辞書オブジェクトを返す.
+    """ [FUNCTIONS] INI形式の設定ファイルを読み込んでConfigParserオブジェクト化し,
+    必須パラメタ有無のチェック, 値の範囲チェックを実施し, ConfigParserオブジェクトを返す.
     必須パラメタ有無チェックおよび値の範囲チェックに問題がある場合は、
     ValueErrorをraiseする.
 
     Keyword arguments:
     file_path -- 設定ファイル(INI形式)パス(絶対パス, あるいはコマンド実行場所からの相対パス)
 
-    Return value: 設定値(辞書オブジェクト)
+    Return value: 設定値(ConfigParserオブジェクト)
     """
+
+    if not os.path.exists(file_path):
+        raise OSError
+
+    inifile = configparser.SafeConfigParser()
+    inifile.read(file_path)
+
+    download_dir = inifile.get('yolp', 'download_dir')
+    if not (os.access(download_dir, os.W_OK)):
+        raise ValueError
+
+    after_minutes = int(inifile.get('weather', 'after_minutes'))
+    if after_minutes < 0 or after_minutes > 60:
+        raise ValueError
+
+    rainfall_threshold = float(inifile.get('weather', 'rainfall_threshold'))
+    if rainfall_threshold < 0.0:
+        raise ValueError
+
+    return inifile
 
 
 def get_weather_information(settings):
     """ [FUNCTIONS] YOLP(気象情報)から気象情報をJSON形式で取得し,
     辞書オブジェクト化して返す.
+    取得したJSONは指定ディレクトリ(設定ファイル)に保存する.
 
     Keyword arguments:
-    settings -- 辞書オブジェクト化した設定値(load_setting_file関数にて得られる値)
+    settings -- 設定値(ConfigParserオブジェクト)
 
     Return value: YOLP(気象情報)から取得したデータ(辞書オブジェクト)
     """
+
+    appid = settings.get('yolp', 'appid')
+    coordinates = settings.get('yolp', 'coordinates')
+    date = datetime.now().strftime('%Y%m%d%H%M')
+
+    url = \
+        "https://map.yahooapis.jp/weather/V1/place?" \
+        "appid={appid}&" \
+        "coordinates={coordinates}&" \
+        "output=json&" \
+        "date={date}&" \
+        "past=0&" \
+        "interval=10".format(appid=appid, coordinates=coordinates, date=date)
+
+    weather_json = requests.get(url).json()
+
+    output_json_file = open(
+        settings.get('yolp', 'download_dir') + "/%s.json" % date, 'w')
+    json.dump(
+        weather_json,
+        output_json_file,
+        indent=4, separators=(',', ': '), ensure_ascii=False)
+
+    output_json_file.close
+
+    return weather_json
 
 
 def parse_weather_information(weather_information_json, settings):
@@ -43,7 +94,7 @@ def parse_weather_information(weather_information_json, settings):
 
     Keywork arguments:
     weather_information_json -- YOLP(気象情報)のデータ(辞書オブジェクト)
-    settings -- 辞書オブジェクト化した設定値(load_setting_file関数にて得られる値)
+    settings -- 設定値(ConfigParserオブジェクト)
     """
 
 
@@ -52,4 +103,5 @@ if __name__ == '__main__':
     # 設定ファイル読み込み
     # YOLP(気象情報呼び出し)
     # 気象情報解析
-    print "c"
+    settings = load_setting_file("./tests/data/settings_normal.ini")
+    json = get_weather_information(settings)
